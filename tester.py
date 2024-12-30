7bf77f97942bbd4efda194335c3a329df81ee86c
Wiktor Jelen <35398584+Akinzou@users.noreply.github.com>
2024-12-30 03:50:19 +0100

    Delete tester.py

--- tester.py
+++
@@ -1,8 +0,0 @@
-import sys
-sl = int(sys.argv[1])*-1
-file_name = sys.argv[2]
-print("min: ", sl)
-print("nazwa: ", file_name)
-
-with open(sys.argv[2], 'a') as plik:
-    plik.write(str(sl) + '\n')
